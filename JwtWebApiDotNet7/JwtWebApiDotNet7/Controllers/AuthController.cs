b2ef904fad68268531b3bb797f771a27ff67ba34
Robert <59577889+robertververs@users.noreply.github.com>
2023-03-07 20:28:41 +0100

    Json Web Token by Patrick God, Youtube: https://www.youtube.com/watch?v=UwruwHl3BlU

---
+++ JwtWebApiDotNet7/JwtWebApiDotNet7/Controllers/AuthController.cs
@@ -0,0 +1,71 @@
+﻿
+
+
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace JwtWebApiDotNet7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        public static User user = new User() { };
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            string passswordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+            
+            user.Username = request.Username;
+            user.PasswordHash = passswordHash;
+            
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<User> Login(UserDto request)
+        {
+            if(user.Username != request.Username)
+            {
+                return BadRequest("User not found.");
+            }
+
+            if(!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+            {
+                return BadRequest("Invalid password.");
+            }
+
+            return Ok(user);
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim> 
+            { 
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+    }
+}
---
+++ JwtWebApiDotNet7/JwtWebApiDotNet7/Program.cs
@@ -0,0 +1,42 @@
+global using JwtWebApiDotNet7.Models;
+global using JwtWebApiDotNet7.Controllers;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Reflection.Metadata.Ecma335;
+global using System.Security.Claims;
+global using Microsoft.IdentityModel.Tokens;
+
+namespace JwtWebApiDotNet7
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
