bc7e15abed69cb93e692fd28a6669b3a07412777
Robert <59577889+robertververs@users.noreply.github.com>
2023-04-20 16:33:26 +0200

    commentaar met de dotnet user-jwts commands veilig gesteld

--- JwtWebApiDotNet7/Controllers/WeatherForecastController.cs
+++ JwtWebApiDotNet7/Controllers/WeatherForecastController.cs
@@ -1,3 +1,13 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+/*
+    dotnet user-jwts create  ID: e2b29d17
+    dotnet user-jwts key     Signing key: 2oWlQ/XPQ6m2QgHuniKbAOD7pOVsthkAtA3hGmn1KLc=
+                            Token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6InJvYmVyIiwic3ViIjoicm9iZXIiLCJqdGkiOiJlMmIyOWQxNyIsImF1ZCI6WyJodHRwOi8vbG9jYWxob3N0OjM2MzI0IiwiaHR0cHM6Ly9sb2NhbGhvc3Q6NDQzMzAiLCJodHRwOi8vbG9jYWxob3N0OjUxMDgiLCJodHRwczovL2xvY2FsaG9zdDo3MDY0Il0sIm5iZiI6MTY4MjAwMDk4OSwiZXhwIjoxNjg5ODYzMzg5LCJpYXQiOjE2ODIwMDA5ODksImlzcyI6ImRvdG5ldC11c2VyLWp3dHMifQ.hn1FxApnnTsKkRxssAD542lUE17sfWvT8QQed7sVp9Y
+    dotnet user-secrets list 
+
+ */
+
 namespace JwtWebApiDotNet7.Controllers
 {
     [ApiController]
@@ -16,7 +26,7 @@ namespace JwtWebApiDotNet7.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast"), Authorize(Roles ="Admin,User")]
+        [HttpGet(Name = "GetWeatherForecast"), Authorize(Roles = "Admin,User")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
